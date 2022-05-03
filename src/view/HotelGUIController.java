aa23c048a09839bc0eceff301ae36f93a6efb8cc
rajipkanxo01 <82520479+rajipkanxo01@users.noreply.github.com>
2022-05-03 13:00:40 +0200

    Added functionality to "next" button and dates

--- src/view/HotelGUIController.java
+++ src/view/HotelGUIController.java
@@ -1,8 +1,28 @@
 package view;
 
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 public class HotelGUIController
 {
+  @FXML DatePicker roomArrivalDate;
+  @FXML DatePicker roomDepartureDate;
+  @FXML DatePicker createArrivalDate;
+  @FXML DatePicker createDepartureDate;
+  @FXML TabPane tabPane;
+  @FXML Tab roomStatus;
+  @FXML Tab createBooking;
 
+
+  public void next(ActionEvent actionEvent)
+  {
+    SingleSelectionModel<Tab> selectionModel = tabPane.getSelectionModel();
+    selectionModel.select(createBooking);
+    createArrivalDate.setValue(roomArrivalDate.getValue());
+    createDepartureDate.setValue(roomDepartureDate.getValue());
+  }
 }
