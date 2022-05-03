d57805d6ba002aec15d61a25d204f8d94cb695c8
royby95 <rodrigorg95@gmail.com>
2022-05-03 18:31:40 +0200

    Change SingleRoom

--- src/model/Room.java
+++ src/model/Room.java
@@ -4,16 +4,14 @@ package model;
 public class Room
 {
   private double price;
-  private String type;
   private boolean smoking;
   private boolean available;
   private int roomNumber;
 
 
-  public Room(double price, String type, boolean smoking, boolean available, int roomNumber)
+  public Room(double price, boolean smoking, boolean available, int roomNumber)
   {
     this.price = price;
-    this.type = type;
     this.smoking = smoking;
     this.available = available;
     this.roomNumber = roomNumber;
--- src/model/SingleRoom.java
+++ src/model/SingleRoom.java
@@ -1,4 +1,24 @@
 package model;
 
+public class SingleRoom extends Room
+{
+  private String type;
+
+  public SingleRoom(double price, boolean smoking, boolean available,
+      int roomNumber, String type)
+  {
+    super(price, smoking, available, roomNumber);
+    this.type = type;
+  }
+
+  public void setType(String type)
+  {
+    this.type = type;
+  }
+  public String getType()
+  {
+    return type;
+  }
+}
 
 
