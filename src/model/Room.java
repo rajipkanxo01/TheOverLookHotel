94b2ebed0867fc7a0348e3e8a25ec8b8b1d8e177
royby95 <rodrigorg95@gmail.com>
2022-05-03 14:43:29 +0200

    created RoomClass:Rodrigo

---
+++ src/model/Room.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Room
+{
+  private double price;
+  private String type;
+  private boolean smoking;
+  private boolean available;
+  private String roomNumber;
+
+  public Room(double price, String type, boolean smoking, boolean available, String roomNumber)
+  {
+    this.price = price;
+    this.type = type;
+    this.smoking = smoking;
+    this.available = available;
+    this.roomNumber = roomNumber;
+
+  }
+
+  public void setRoomNumber
+
+}
