3a21a12a4d184af9311e43476c7478fb62271bbd
rajipkanxo01 <82520479+rajipkanxo01@users.noreply.github.com>
2022-05-09 15:30:25 +0200

    test class

---
+++ src/TestClass.java
@@ -0,0 +1,9 @@
+import model.HotelManager;
+
+public class TestClass
+{
+  public static void main(String[] args)
+  {
+    HotelManager manager = new HotelManager("rooms.bin" , "guests.bin");
+  }
+}
