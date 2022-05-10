a161d4f1ccb6d360fd3ad06cada4c1cdc393611c
rajipkanxo01 <82520479+rajipkanxo01@users.noreply.github.com>
2022-05-10 12:18:25 +0200

    Added getAllAvailableRooms method in model manager

--- src/TestClass.java
+++ src/TestClass.java
@@ -10,9 +10,10 @@ public class TestClass
   {
     HotelModelManager manager = new HotelModelManager("rooms.bin" , "guests.bin");
     manager.addRooms();
-    RoomList temp = manager.getAllRooms();
-    System.out.println(temp);
+//    RoomList temp = manager.getAllRooms();
+    System.out.println(manager.getAllAvailableRooms(LocalDate.now(),LocalDate.parse("2022-05-15")));
     manager.addGuest(new Guest("first name" , "last name" , "address", "12" , "nepal" ,
         LocalDate.parse("2002-03-08")));
+    System.out.println(LocalDate.parse("2002-03-08"));;
   }
 }
